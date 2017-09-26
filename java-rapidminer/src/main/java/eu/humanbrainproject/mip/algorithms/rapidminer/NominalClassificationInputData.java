c1a58013f675a06595fd86c95cf4cee37e3d6665
Ludovic Claude <ludovic.claude54@gmail.com>
2017-09-26 17:19:07 +0200

    Add input data helper for nominal classification

---
+++ java-rapidminer/src/main/java/eu/humanbrainproject/mip/algorithms/rapidminer/NominalClassificationInputData.java
@@ -0,0 +1,58 @@
+package eu.humanbrainproject.mip.algorithms.rapidminer;
+
+import com.rapidminer.example.Attribute;
+import com.rapidminer.example.ExampleSet;
+import com.rapidminer.example.table.AttributeFactory;
+import com.rapidminer.example.table.DoubleArrayDataRow;
+import com.rapidminer.example.table.MemoryExampleTable;
+import com.rapidminer.tools.Ontology;
+import eu.humanbrainproject.mip.algorithms.db.DBException;
+
+import java.util.LinkedList;
+import java.util.List;
+
+/**
+ * Useful for tests
+ */
+public class NominalClassificationInputData extends InputData {
+
+    private final String[][] sampleData;
+    private final String[] labels;
+
+    public NominalClassificationInputData(String[] featuresNames, String variableName, String[][] sampleData, String[] labels) {
+        super(featuresNames, variableName, null);
+
+        this.sampleData = sampleData;
+        this.labels = labels;
+    }
+
+    protected ExampleSet createExampleSet() throws DBException {
+
+        List<Attribute> attributes = new LinkedList<>();
+        for (String featuresName : getFeaturesNames()) {
+            attributes.add(AttributeFactory.createAttribute(featuresName, Ontology.REAL));
+        }
+
+        // Create label
+        Attribute label = AttributeFactory.createAttribute(getVariableName(), Ontology.NOMINAL);
+        attributes.add(label);
+
+        // Create table
+        MemoryExampleTable table = new MemoryExampleTable(attributes);
+
+        // Fill the table
+        for (int d = 0; d < sampleData.length; d++) {
+            double[] tableData = new double[attributes.size()];
+            System.arraycopy(sampleData[d], 0, tableData, 0, sampleData[d].length);
+
+            // Maps the nominal classification to a double value
+            tableData[sampleData[d].length] = label.getMapping().mapString(labels[d]);
+
+            // Add data row
+            table.addDataRow(new DoubleArrayDataRow(tableData));
+        }
+
+        // Create example set
+        return table.createExampleSet(label);
+    }
+}
