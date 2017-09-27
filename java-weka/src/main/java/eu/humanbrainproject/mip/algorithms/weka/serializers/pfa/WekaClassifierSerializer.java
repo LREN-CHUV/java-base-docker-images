27c7cd8223dc56df45ce742d9efee2e0c4edfb61
Ludovic Claude <ludovic.claude@laposte.net>
2017-09-28 00:05:03 +0200

    Weka: WIP simple linear regression

--- java-weka/src/main/java/eu/humanbrainproject/mip/algorithms/weka/serializers/pfa/WekaClassifierSerializer.java
+++ java-weka/src/main/java/eu/humanbrainproject/mip/algorithms/weka/serializers/pfa/WekaClassifierSerializer.java
@@ -3,10 +3,17 @@ package eu.humanbrainproject.mip.algorithms.weka.serializers.pfa;
 import com.fasterxml.jackson.core.JsonGenerator;
 import eu.humanbrainproject.mip.algorithms.weka.WekaClassifier;
 import weka.classifiers.Classifier;
+import weka.classifiers.functions.SimpleLinearRegression;
 
 import java.io.IOException;
+import java.lang.reflect.Field;
+import java.util.logging.Level;
+import java.util.logging.Logger;
 
 public abstract class WekaClassifierSerializer<M extends Classifier> {
+
+    private static final Logger LOGGER = Logger.getLogger(WekaClassifierSerializer.class.getName());
+
 
     public abstract void writeModelConstants(WekaClassifier<M> model, JsonGenerator jgen) throws IOException;
 
@@ -29,4 +36,23 @@ public abstract class WekaClassifierSerializer<M extends Classifier> {
     public void writePfaPools(WekaClassifier<M> model, JsonGenerator jgen) throws IOException {
         // Empty set of pools, to override if necessary
     }
+
+    @SuppressWarnings("unchecked")
+    protected <T> T accessPrivateField(M model, String fieldName) {
+        //TODO Remove this dirty and dangerous trick
+
+        Class<? extends Classifier> modelClass = model.getClass();
+        try {
+            Field field = modelClass.getDeclaredField(fieldName);
+            field.setAccessible(true);
+            return (T) field.get(model);
+
+        } catch (NoSuchFieldException | IllegalAccessException e) {
+            String msg = "Cannot access private field " + fieldName + " from model " + modelClass;
+            LOGGER.log(Level.SEVERE, msg, e);
+            throw new RuntimeException(msg, e);
+        }
+    }
+
+
 }
--- java-weka/src/test/java/eu/humanbrainproject/mip/algorithms/weka/simplelr/SimpleLinearRegressionSerializer.java
+++ java-weka/src/test/java/eu/humanbrainproject/mip/algorithms/weka/simplelr/SimpleLinearRegressionSerializer.java
@@ -3,65 +3,70 @@ package eu.humanbrainproject.mip.algorithms.weka.simplelr;
 import com.fasterxml.jackson.core.JsonGenerator;
 import eu.humanbrainproject.mip.algorithms.weka.WekaClassifier;
 import eu.humanbrainproject.mip.algorithms.weka.serializers.pfa.WekaClassifierSerializer;
+import org.apache.avro.Schema;
+import org.apache.avro.SchemaBuilder;
 import weka.classifiers.functions.SimpleLinearRegression;
+import weka.core.Attribute;
 
 import java.io.IOException;
+import java.lang.reflect.Field;
+import java.util.logging.Level;
+import java.util.logging.Logger;
 
 public class SimpleLinearRegressionSerializer extends WekaClassifierSerializer<SimpleLinearRegression> {
 
     @Override
     public void writeModelConstants(WekaClassifier<SimpleLinearRegression> model, JsonGenerator jgen) throws IOException {
         final SimpleLinearRegression lr = model.getTrainedClassifier();
-        lr.getSlope()
 
         jgen.writeObjectFieldStart("model");
+        {
+            jgen.writeObjectFieldStart("type");
             {
-                jgen.writeObjectFieldStart("type");
+                jgen.writeStringField("doc", "The model parameter");
+                jgen.writeStringField("name", "value");
+                jgen.writeFieldName("type");
                 {
-                    jgen.writeStringField("doc", "The model parameter");
-                    jgen.writeStringField("name", "value");
-                    jgen.writeStringField("type", (method.equals("mode")) ? "string" : "double");
-                }
-                jgen.writeEndObject();
+                    Schema schema = SchemaBuilder.record("LinearModel").fields()
+                                /* */.name("slope").type().doubleType().noDefault()
+                                /* */.name("intercept").type().doubleType().noDefault()
+                                /* */.name("classMeanForMissing").type().doubleType().noDefault()
+                                /* */.name("selectedVariable").type().stringType().stringDefault(null)
+                            .endRecord();
 
-                switch (method) {
-                    case "median":
-                    case "average":
-                    case "constant":
-                        jgen.writeNumberField("init", model.getTrainedModel().getValue());
-                        break;
-                    case "mode":
-                        jgen.writeStringField("init", String.valueOf(model.getTrainedModel().getValue()));
-                        break;
-                    default:
-                        throw new IllegalArgumentException("Cannot handle method: " + method);
+                    jgen.writeRawValue(schema.toString());
                 }
             }
             jgen.writeEndObject();
+
+            jgen.writeObjectFieldStart("init");
+            {
+                jgen.writeNumberField("intercept", lr.getIntercept());
+                if (lr.foundUsefulAttribute()) {
+                    Attribute attribute = accessPrivateField(lr, "m_attribute");
+                    Double classMeanForMissing = accessPrivateField(lr, "m_classMeanForMissing");
+
+                    jgen.writeNumberField("slope", lr.getSlope());
+                    jgen.writeNumberField("classMeanForMissing", classMeanForMissing);
+                    jgen.writeStringField("selectedVariable", attribute.name());
+                }
+            }
+        }
+        jgen.writeEndObject();
     }
 
     @Override
     public void writePfaAction(WekaClassifier<SimpleLinearRegression> model, JsonGenerator jgen) throws IOException {
-        String method = model.getParameters().get("method");
-        if (!method.equals("attribute")) {
-            jgen.writeStartObject();
+        jgen.writeStartObject();
+        {
+            jgen.writeStringField("attr", "input");
+            jgen.writeArrayFieldStart("path");
             {
-                jgen.writeStringField("cell", "model");
+                jgen.writeString(attributeName);
             }
-            jgen.writeEndObject();
-        } else {
-            String attributeName = model.getParameters().get("attribute_name");
-
-            jgen.writeStartObject();
-            {
-                jgen.writeStringField("attr", "input");
-                jgen.writeArrayFieldStart("path");
-                {
-                    jgen.writeString(attributeName);
-                }
-                jgen.writeEndArray();
-            }
-            jgen.writeEndObject();
+            jgen.writeEndArray();
         }
+        jgen.writeEndObject();
     }
+
 }
