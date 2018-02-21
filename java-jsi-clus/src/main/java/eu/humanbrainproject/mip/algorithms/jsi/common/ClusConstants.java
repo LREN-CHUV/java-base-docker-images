061d40d4080633872d89efed541159801b6717f6
Martin Breskvar <martin.breskvar@ijs.si>
2018-02-22 00:43:28 +0100

    improved java-jsi-clus library

--- java-jsi-clus/src/main/java/eu/humanbrainproject/mip/algorithms/jsi/common/ClusConstants.java
+++ java-jsi-clus/src/main/java/eu/humanbrainproject/mip/algorithms/jsi/common/ClusConstants.java
@@ -8,19 +8,22 @@ package eu.humanbrainproject.mip.algorithms.jsi.common;
  */
 public class ClusConstants {
 
-    /** the name of all files regarding clus (this can be changed) */
+    /** the name of all files regarding CLUS (this can be changed) */
     private static final String CLUS_FILE = "experiment";
 
-    /** where should the arff be stored (clus specific settings, do not change this!) */
+    /** where should the arff be stored (CLUS specific settings, do not change this!) */
     public static final String CLUS_DATAFILE = CLUS_FILE + ".arff";
 
-    /** where should the settings file be stored (clus specific settings, do not change this!) */
+    /** where should the settings file be stored (CLUS specific settings, do not change this!) */
     public static final String CLUS_SETTINGSFILE = CLUS_FILE + ".s";
 
-    /** where should the .out file be stored (clus specific settings, do not change this!) */
+    /** where should the .out file be stored (CLUS specific settings, do not change this!) */
     public static final String CLUS_OUTFILE = CLUS_FILE + ".out";
 
-    /** where should the .model file be stored (clus specific settings, do not change this!) */
+    /** where should the .model file be stored (CLUS specific settings, do not change this!) */
     public static final String CLUS_MODELFILE = CLUS_FILE + ".model";
+    
+    /** where should the .fimp file be stored (CLUS specific settings, do not change this!) */
+    public static final String CLUS_FIMPFILE = CLUS_FILE + ".fimp";
 
 }
