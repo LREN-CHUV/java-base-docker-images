69cd9b5da51e274fa013a91adfde358fbd7ca96e
Martin Breskvar <martin.breskvar@ijs.si>
2018-03-03 02:56:24 +0100

    java-mip ResultsFormat and InputDescription update

--- java-mip/src/main/java/eu/humanbrainproject/mip/algorithms/ResultsFormat.java
+++ java-mip/src/main/java/eu/humanbrainproject/mip/algorithms/ResultsFormat.java
@@ -1,31 +1,32 @@
 package eu.humanbrainproject.mip.algorithms;
 
 /**
- * As the algorithm results are exported as a generic text, we define here what was the format used to encode the results.
+ * As the algorithm results are exported as a generic text, we define here what was the format used
+ * to encode the results.
  */
 public enum ResultsFormat {
-    PFA_JSON("pfa_json", "application/pfa+json"),
-    PFA_YAML("pfa_yaml", "application/pfa+yaml"),
-    HIGHCHARTS("highcharts_json", "application/highcharts+json"),
-    JAVASCRIPT_VISJS("visjs", "application/visjs+javascript"),
-    PNG_IMAGE("png_image", "image/png;base64"),
-    SVG_IMAGE("svg_image", "image/svg+xml"),
-    TABULAR_DATA_RESOURCE_JSON("tdr_json", "application/json");
+  PFA_JSON("pfa_json", "application/pfa+json"),
+  PFA_YAML("pfa_yaml", "application/pfa+yaml"),
+  HIGHCHARTS("highcharts_json", "application/highcharts+json"),
+  JAVASCRIPT_VISJS("visjs", "application/visjs+javascript"),
+  PNG_IMAGE("png_image", "image/png;base64"),
+  SVG_IMAGE("svg_image", "image/svg+xml"),
+  TABULAR_DATA_RESOURCE_JSON("tdr_json", "application/json"),
+  HTML("html", "text/html");
 
-    private final String shape;
-    private final String mimeType;
+  private final String shape;
+  private final String mimeType;
 
-    ResultsFormat(String shape, String mimeType) {
-        this.shape = shape;
-        this.mimeType = mimeType;
-    }
+  ResultsFormat(String shape, String mimeType) {
+    this.shape = shape;
+    this.mimeType = mimeType;
+  }
 
-    public String getShape() {
-        return shape;
-    }
+  public String getShape() {
+    return shape;
+  }
 
-    public String getMimeType() {
-        return mimeType;
-    }
-
+  public String getMimeType() {
+    return mimeType;
+  }
 }
